4c5bd8e9e7d80c1f114c9f8f0cc70b2fb0fdb10a
Delfynn2kx <hubert.delphine@gmail.com>
2023-09-13 20:45:08 -0400

    V7 serviceW

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-//Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v5';
-//Add list of files to cache here.
+const CACHE_NAME = 'static-cache-v7';
+
 const FILES_TO_CACHE = [
-    'offline.html', 
+    
     'index.html',
+    'offline.html',
     'activites.html',
     'confirmation.html',
     'devenir-partenaires.html',
@@ -37,79 +37,44 @@ const FILES_TO_CACHE = [
     'tailwind.config.js',
     'css/style.css'
 ];
+
 self.addEventListener('install', (evt) => {
-console.log('[ServiceWorker] Install');
-// Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-    console.log('[ServiceWorker] Pre-caching offline page');
-    return cache.addAll(FILES_TO_CACHE);
-    })
+    console.log('[ServiceWorker] Install');
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
     self.skipWaiting();
 });
+
 self.addEventListener('activate', (evt) => {
-console.log('[ServiceWorker] Activate');
-//Remove previous cached data from disk.
-self.clients.claim();
-});
-self.addEventListener('fetch', (evt) => {
-console.log('[ServiceWorker] Fetch', evt.request.url);
-//Add fetch event handler here.
-});
-
-
-
-
-    self.addEventListener('activate', (evt) => {
-        console.log('[ServiceWorker] Activate');
-        //Remove previous cached data from disk.
-        evt.waitUntil(
+    console.log('[ServiceWorker] Activate');
+    evt.waitUntil(
         caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-        console.log('[ServiceWorker] Removing old cache',
-        key);
-        return caches.delete(key);
-        }
-        }));
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
         })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    if (evt.request.mode === 'navigate') {
+        evt.respondWith(
+            fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('index.html');
+                    });
+            })
         );
-        self.clients.claim();
-
-    self.addEventListener('fetch', (evt) => {
-console.log('[ServiceWorker] Fetch', evt.request.url);
-//Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-// Not a page navigation, bail.
-return;
-}
-evt.respondWith(
-fetch(evt.request)
-.catch(() => {
-return caches.open(CACHE_NAME)
-.then((cache) => {
-return cache.match('./TP3_HubertDelphine/index.html' );
-});
-})
-);
-});
-        });
-
-    self.addEventListener('fetch', (evt) => {
-console.log('[ServiceWorker] Fetch', evt.request.url);
-//Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-// Not a page navigation, bail.
-return;
-}
-evt.respondWith(
-fetch(evt.request)
-.catch(() => {
-return caches.open(CACHE_NAME)
-.then((cache) => {
-return cache.match('./TP3_HubertDelphine/index.html' );
-});
-})
-);
+    }
 });
